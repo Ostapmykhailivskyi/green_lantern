1fc614d47c937c62c829785206044e06991bd547
codePanda <panda.597.ostap@gmail.com>
2020-03-31 19:10:30 +0300

    added TDD gomework, fixed OOP homework

--- lantern/tdd/lonely_robot.py
+++ lantern/tdd/lonely_robot.py
@@ -5,15 +5,80 @@ class Asteroid:
 
 
 class Robot:
-    def __init__(self, x, y, asteroid):
+    def __init__(self, x, y, asteroid, direction):
         self.x = x
         self.y = y
+        self.direction = direction
         self.asteroid = asteroid
         if self.x > self.asteroid.x:
             raise MissAsteroidError()
         if self.y > self.asteroid.y:
-            raise MissAsteroidError
+            raise MissAsteroidError()
+
+    def turn_left(self):
+        turns = {"E": "N",
+                 "N": "W",
+                 "W": "S",
+                 "S": "E", }
+        self.direction = turns[self.direction]
+
+    def turn_right(self):
+        turns = {"N": "E",
+                 "E": "S",
+                 "S": "W",
+                 "W": "N", }
+        self.direction = turns[self.direction]
+
+    def move_forward(self, steps):
+        if self.direction == "E":
+            if self.x + steps > self.asteroid.x:
+                raise ValueError("Robot fell from asteroid")
+            else:
+                self.x += steps
+        elif self.direction == "N":
+            if self.y + steps > self.asteroid.y:
+                raise ValueError("Robot fell from asteroid")
+            else:
+                self.y += steps
+        elif self.direction == "W":
+            if (self.x - steps) < 0:
+                raise ValueError("Robot fell from asteroid")
+            else:
+                self.x -= steps
+        elif self.direction == "S":
+            if self.y - steps < 0:
+                raise ValueError("Robot fell from asteroid")
+            else:
+                self.y -= steps
+
+    def move_backward(self, steps):
+        if self.direction == "E":
+            if self.x - steps < 0:
+                raise ValueError("Robot fell from asteroid")
+            else:
+                self.x -= steps
+        if self.direction == "N":
+            if self.y - steps < 0:
+                raise ValueError("Robot fell from asteroid")
+            else:
+                self.y -= steps
+        if self.direction == "W":
+            if self.y + steps > self.asteroid.y:
+                raise ValueError("Robot fell from asteroid")
+            else:
+                self.y += steps
+        if self.direction == "S":
+            if self.x + steps > self.asteroid.x:
+                raise ValueError("Robot fell from asteroid")
+            else:
+                self.x += steps
 
 
 class MissAsteroidError(Exception):
-    pass
+    print("Robot missed above asteroid")
+
+
+if __name__ == '__main__':
+    asteroid = Asteroid(20, 30)
+    robot = Robot(10, 13, asteroid, "E")
+    robot.move_forward(20)
