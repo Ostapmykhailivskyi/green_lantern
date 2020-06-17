aebcbfbdc2d2afc0e21017cb1c5300db2647b0e1
codePanda <panda.597.ostap@gmail.com>
2020-06-17 22:42:00 +0300

    done 7-8 hw

--- lantern/flask/flask_login_and_templates/practice/grocery_store/routes/__init__.py
+++ lantern/flask/flask_login_and_templates/practice/grocery_store/routes/__init__.py
@@ -1,6 +1,5 @@
 from flask import Blueprint
 from flask_restful import Api
-
 from grocery_store.routes.users import Users
 from grocery_store.routes.goods import Goods
 from grocery_store.routes.stores import Stores
@@ -18,5 +17,4 @@ api_stores = Api(stores)
 api_users.add_resource(Users, "/users", "/users/<user_id>")
 api_goods.add_resource(Goods, "/goods", "/goods/<good_id>")
 api_stores.add_resource(Stores, "/stores", "/stores/<store_id>")
-
 __all__ = ['users', 'goods', 'stores', 'auth', 'main']
--- lantern/flask/flask_login_and_templates/practice/grocery_store/routes/main.py
+++ lantern/flask/flask_login_and_templates/practice/grocery_store/routes/main.py
@@ -1,6 +1,10 @@
-from flask import Blueprint, render_template
+import json
+from datetime import datetime
+from pprint import pprint
+from flask import Blueprint, render_template, jsonify
 from grocery_store.database import db
 from flask_login import current_user, login_required
+from grocery_store.models import Good, Store, User, Order, OrderLine
 
 main = Blueprint('main', __name__)
 
@@ -13,4 +17,53 @@ def index():
 @main.route('/profile')
 @login_required
 def profile():
-    return render_template('profile.html', user=current_user.name, email=current_user.email)
+    stores = Store.query.filter(Store.manager_id == current_user.user_id).all()
+    return render_template('profile.html',
+                           user=current_user.name,
+                           email=current_user.email,
+                           stores=stores)
+
+
+@main.route('/good', methods=['GET'])
+def gd():
+    return render_template('goods.html', items=Good.query.all())
+
+
+@main.route('/store', methods=['GET'])
+def st():
+    return render_template('store.html', items=Store.query.all())
+
+
+@main.route('/st_detail/<name>')
+def detail(name=None):
+    if name:
+        return render_template('detail.html', items=Store.query.filter_by(name=name))
+    return render_template('detail.html')
+
+
+@main.route('/orders')
+@login_required
+def orders():
+    orders = Order.query.filter(Order.user_id == current_user.user_id).all()
+    order_ = {}
+
+    for order in orders:
+        a = []
+        b = []
+        order_[order.order_id] = {}
+        order_[order.order_id]['date'] = order.created_time
+        store = Store.query.filter(Store.store_id == order.store_id).first()
+        order_[order.order_id]['store_n'] = store.name
+        order_[order.order_id]['name_g'] = {}
+        order_lines = OrderLine.query.filter(order.order_id == OrderLine.order_id).all()
+        for order_line in order_lines:
+            goods = Good.query.filter(Good.good_id == order_line.good_id).all()
+            for good in goods:
+                a.append(good.name)
+                b.append(good.price)
+                suma = sum(b)
+                order_[order.order_id]['name_g'] = dict(zip(a[::], b[::]))
+        order_[order.order_id]['sum'] = suma
+
+    return render_template('orders.html',
+                           orders=order_)
