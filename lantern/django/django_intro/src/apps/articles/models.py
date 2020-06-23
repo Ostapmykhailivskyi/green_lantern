039f1a373dc34831bcd3067a27ff7e08c6c3d473
codePanda <panda.597.ostap@gmail.com>
2020-06-23 22:57:01 +0300

    done 11 HW

--- lantern/django/django_intro/src/apps/articles/models.py
+++ lantern/django/django_intro/src/apps/articles/models.py
@@ -1,8 +1,8 @@
 from django.conf import settings
+from django.contrib.auth.models import User
 from django.db import models
-
-
-# Create your models here.
+from django.dispatch import receiver
+from django.utils.translation import gettext as _
 
 
 class Tag(models.Model):
@@ -10,9 +10,18 @@ class Tag(models.Model):
 
 
 class Article(models.Model):
+    STATUS_PENDING = 'pending'
+    STATUS_APPROVED = 'approved'
+    STATUS_DECLINED = 'declined'
+
+    STATUS_CHOICES = (
+        (STATUS_PENDING, _('Pending')),
+        (STATUS_APPROVED, _('Approved')),
+        (STATUS_DECLINED, _('Declined'))
+    )
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
     title = models.CharField(max_length=255, verbose_name='Title', db_index=True)
     body = models.TextField(max_length=5000, verbose_name='Article body')
     tags = models.ManyToManyField(to='Tag', related_name='articles', blank=True)
@@ -22,3 +31,16 @@ class Article(models.Model):
         on_delete=models.SET_NULL,
         related_name='articles'
     )
+    image = models.ImageField(upload_to='articles', null=True, blank=True)
+
+    class Meta:
+        permissions = [
+            ('can_approve', 'Can approve'),
+            ('can_decline', 'Can decline')
+        ]
+
+
+@receiver(models.signals.pre_save, sender=Article)
+def upper_handler(sender, **kwargs):
+    kwargs['instance'].body = kwargs['instance'].body.upper()
+    # send_newsletters()
--- lantern/django/django_intro/src/apps/articles/views.py
+++ lantern/django/django_intro/src/apps/articles/views.py
@@ -1,21 +1,50 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.sessions.models import Session
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 # Create your views here.
+from django.urls import reverse
+from django.utils.decorators import method_decorator
 from django.views import View
+from django.views.decorators.cache import cache_page
+from django.views.generic import TemplateView, DetailView, FormView, ListView, CreateView, UpdateView, DeleteView
 
-from apps.articles.forms import SearchForm
-from apps.articles.models import Article
+from .forms import ArticleImageForm, ArticleForm
+from .models import Article
+from .utils import get_articles
 
 
-def main_page(request):
+def main_page(request, some_id=None, *args, **kwargs):
     return render(request, 'pages/main_page.html')
 
 
+@login_required
+def main_page_logged_id(request, some_id=None, *args, **kwargs):
+    return render(request, 'pages/main_page.html')
+
+
+class IndexView(ListView):
+    model = Article(title='randomname')
+    template_name = 'index.html'
+    context_object_name = 'articles'
+
+    def get_queryset(self):
+        return Article.objects.all()
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(IndexView, self).get_context_data()
+        context['page_title'] = 'All titles'
+        return context
+
+
 class SearchResultsView(View):
+
+    # @method_decorator(cache_page(60))
     def get(self, request, **kwargs):
         # form = SearchForm(data=request.GET)
         search_q = request.GET.get('search', '')
         if search_q:
-            articles = Article.objects.filter(title__icontains=search_q)
+            articles = get_articles(search_q, 120)
         else:
             articles = Article.objects.all()
 
@@ -24,3 +53,72 @@ class SearchResultsView(View):
             # 'search_form': form
         }
         return render(request, 'pages/search.html', context=context_data)
+
+    def post(self, request):
+        return HttpResponse('{}', status=201)
+
+
+class ShowTitsView(TemplateView):
+    template_name = 'tits_list.html'
+
+    def get_context_data(self, **kwargs):
+        # self.request
+        return {
+            'tits': 2
+        }
+
+
+class ArticleCreateView(CreateView):
+    model = Article
+    template_name = 'create.html'
+    form_class = ArticleForm
+
+    def get_success_url(self):
+        return reverse('detail', args=(self.object.id,))
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    context_object_name = 'article'
+    template_name = 'article.html'
+    pk_url_kwarg = 'id'
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['tits'] = 42
+        return ctx
+
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    template_name = 'update.html'
+    form_class = ArticleForm
+    pk_url_kwarg = 'article_id'
+
+    def get_success_url(self):
+        return reverse('detail', args=(self.object.id,))
+
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    template_name = 'delete.html'
+    form_class = ArticleForm
+    context_object_name = 'article'
+    pk_url_kwarg = 'article_id'
+
+
+class ArticleUpdateImageView(FormView):
+    form_class = ArticleImageForm
+    template_name = 'article_image-update.html'
+
+    def get_success_url(self):
+        return reverse('articles:detail', kwargs={'id': self.kwargs['id']})
+
+    def form_valid(self, form):
+        # get_object_or_404()
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['id'] = self.kwargs['id']
+        return ctx
